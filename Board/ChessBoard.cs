ad284434de058723c1c0cc9cdd395123b2a147ad
Gustavo Caldas de Souza <133917173+gustavocaldassouza@users.noreply.github.com>
2025-04-28 20:47:39 -0400

    Kaka

--- Board/ChessBoard.cs
+++ Board/ChessBoard.cs
@@ -11,7 +11,35 @@ namespace Board
             Board = new ChessPiece[8, 8];
         }
 
-        public void SetUpBoard() { }
+        public void SetUpBoard()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                Board[1, i] = new Pawn("White");
+                Board[6, i] = new Pawn("Black");
+            }
+
+            Board[0, 0] = new Rook("White");
+            Board[0, 7] = new Rook("White");
+            Board[7, 0] = new Rook("Black");
+            Board[7, 7] = new Rook("Black");
+
+            Board[0, 1] = new Knight("White");
+            Board[0, 6] = new Knight("White");
+            Board[7, 1] = new Knight("Black");
+            Board[7, 6] = new Knight("Black");
+
+            Board[0, 2] = new Bishop("White");
+            Board[0, 5] = new Bishop("White");
+            Board[7, 2] = new Bishop("Black");
+            Board[7, 5] = new Bishop("Black");
+
+            Board[0, 3] = new Queen("White");
+            Board[7, 3] = new Queen("Black");
+
+            Board[0, 4] = new King("White");
+            Board[7, 4] = new King("Black");
+        }
 
         public void DisplayBoard()
         {
@@ -25,7 +53,7 @@ namespace Board
                     }
                     else
                     {
-                        Console.Write(". ");
+                        Console.Write("_");
                     }
                 }
                 Console.WriteLine();
--- GamePieces/Rook.cs
+++ GamePieces/Rook.cs
@@ -13,12 +13,22 @@ namespace GamePieces
 
         public override bool IsValidMove(ChessBoard board, string fromPosition, string toPosition)
         {
-            throw new NotImplementedException();
+            Position positionFrom = new Position(fromPosition[0], fromPosition[1]);
+            Position positionTo = new Position(fromPosition[0], toPosition[1]);
+            List<string> validMoves = new List<string>();
+
+
         }
 
         public override ChessPiece MovePiece(ChessBoard board, string fromPosition, string toPosition)
         {
-            throw new NotImplementedException();
+            ChessPiece? piece = null;
+            if (IsValidMove(board, fromPosition, toPosition))
+            {
+                piece = board.Board[Convert.ToInt32(toPosition[1].ToString()), Convert.ToInt32(toPosition[0].ToString())];
+                return piece;
+            }
+            return piece!;
         }
     }
 }
