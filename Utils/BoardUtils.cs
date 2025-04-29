4d61e885888017ad01b241f9632b8b610057f023
muriloruass <murilolucena15@gmail.com>
2025-04-28 23:48:11 -0400

    Step 3 finished

--- Utils/BoardUtils.cs
+++ Utils/BoardUtils.cs
@@ -7,22 +7,45 @@ namespace Utils
     {
         public static int FileCharToIdx(char fileChar)
         {
-            return fileChar - 'A'; // A=0, B=1, ..., H=7
+            switch (char.ToLower(fileChar))
+            {
+                case 'a': return 0;
+                case 'b': return 1;
+                case 'c': return 2;
+                case 'd': return 3;
+                case 'e': return 4;
+                case 'f': return 5;
+                case 'g': return 6;
+                case 'h': return 7;
+            }
+            return -1;
         }
 
         public static char FileIntToChar(int fileIdx)
         {
-            return (char)('A' + fileIdx); // 0=A, 1=B, ..., 7=H
+            switch (fileIdx)
+            {
+                case 0: return 'A';
+                case 1: return 'B';
+                case 2: return 'C';
+                case 3: return 'D';
+                case 4: return 'E';
+                case 5: return 'F';
+                case 6: return 'G';
+                case 7: return 'H';
+            }
+
+            return ' ';
         }
 
         public static int RankIntToIdx(int rank)
         {
-            return 8 - rank; // 8=0, 1=7
+            return 8 - rank;
         }
 
         public static int RankIdxToInt(int idx)
         {
-            return 8 - idx; // 0=8, 7=1
+            return 8 - idx;
         }
 
         public static ChessPiece PositionToPiece(ChessBoard board, string position)
