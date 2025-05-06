895c127c2b999bc3f91c45efb9bdceb53e9fb294
Gustavo Caldas de Souza <133917173+gustavocaldassouza@users.noreply.github.com>
2025-05-06 06:47:40 -0400

    Implement Queen movement logic in IsValidMove method

--- GamePieces/Queen.cs
+++ GamePieces/Queen.cs
@@ -1,4 +1,5 @@
 using Board;
+using Utils;
 
 namespace GamePieces
 {
@@ -12,7 +13,81 @@ namespace GamePieces
 
         public override bool IsValidMove(ChessBoard board, string fromPosition, string toPosition)
         {
-            throw new NotImplementedException();
+            int fromColIdx = BoardUtils.FileCharToIdx(fromPosition[0]);
+            int fromRowIdx = BoardUtils.RankIntToIdx(int.Parse(fromPosition[1].ToString()));
+
+            var movePositions = new List<string>();
+
+            // up
+            for (int i = 1; i < 8; i++)
+            {
+                string? pos = board.IsValidPosition(this, fromColIdx, fromRowIdx - i);
+                if (pos == string.Empty)
+                    break;
+                movePositions.Add(pos!);
+            }
+            // down
+            for (int i = 1; i < 8; i++)
+            {
+                string? pos = board.IsValidPosition(this, fromColIdx, fromRowIdx + i);
+                if (pos == string.Empty)
+                    break;
+                movePositions.Add(pos!);
+            }
+            // left
+            for (int i = 1; i < 8; i++)
+            {
+                string? pos = board.IsValidPosition(this, fromColIdx - i, fromRowIdx);
+                if (pos == string.Empty)
+                    break;
+                movePositions.Add(pos!);
+            }
+            // right
+            for (int i = 1; i < 8; i++)
+            {
+                string? pos = board.IsValidPosition(this, fromColIdx + i, fromRowIdx);
+                if (pos == string.Empty)
+                    break;
+                movePositions.Add(pos!);
+            }
+            // up-right diagonal
+            for (int i = 1; i < 8; i++)
+            {
+                string? pos = board.IsValidPosition(this, fromColIdx + i, fromRowIdx - i);
+                if (pos == string.Empty)
+                    break;
+                movePositions.Add(pos!);
+            }
+
+            // up-left diagonal
+            for (int i = 1; i < 8; i++)
+            {
+                string? pos = board.IsValidPosition(this, fromColIdx - i, fromRowIdx - i);
+                if (pos == string.Empty)
+                    break;
+                movePositions.Add(pos!);
+            }
+
+            // down-right diagonal
+            for (int i = 1; i < 8; i++)
+            {
+                string? pos = board.IsValidPosition(this, fromColIdx + i, fromRowIdx + i);
+                if (pos == string.Empty)
+                    break;
+                movePositions.Add(pos!);
+            }
+
+            // down-left diagonal
+            for (int i = 1; i < 8; i++)
+            {
+                string? pos = board.IsValidPosition(this, fromColIdx - i, fromRowIdx + i);
+                if (pos == string.Empty)
+                    break;
+                movePositions.Add(pos!);
+            }
+
+            return movePositions.Contains(toPosition.ToUpper());
+
         }
     }
 }
